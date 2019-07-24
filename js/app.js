a7bfa2d65635669a35ae215d4ec652eafa62fb67
Landr11 <citywokb3@yahoo.com>
2019-07-24 10:09:38 +0100

    udate images folder path

--- js/app.js
+++ js/app.js
@@ -41,8 +41,8 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     diceDOM1.style.display = "block";
     diceDOM2.style.display = "block";
     //change the image to reflect the result of the dice roll
-    diceDOM1.src = "dice-" + dice1 + ".png";
-    diceDOM2.src = "dice-" + dice2 + ".png";
+    diceDOM1.src = "./img/dice-" + dice1 + ".png";
+    diceDOM2.src = "./img/dice-" + dice2 + ".png";
 
     //Update the round score IF the rolled number was not 1
     if (dice !== 2 && skip.reduce(getSum) !== 12 && skip.every(checkScore)) {
